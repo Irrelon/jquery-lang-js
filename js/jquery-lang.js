0e639260524907567482d3176836ff656fde195b
Stefano Ciarcià <stephan.warrior@gmail.com>
2013-02-23 12:20:17 +0100

    comments

--- js/jquery-lang.js
+++ js/jquery-lang.js
@@ -1,37 +1,37 @@
 var IgeEventsLite = function () {}
 
 IgeEventsLite.prototype.on = function (evtName, fn) {
-	if (evtName && fn) {
-		this.eventList[evtName] = this.eventList[evtName] || [];
-		this.eventList[evtName].push(fn);
-	}
+  if (evtName && fn) {
+	this.eventList[evtName] = this.eventList[evtName] || [];
+	this.eventList[evtName].push(fn);
+  }
 }
-	
+
 IgeEventsLite.prototype.emit = function (evtName) {
-	if (evtName) {
-		this.eventList = this.eventList || [];
-		var args = [];
-		for (var i = 1; i < arguments.length; i++) {
-			args.push(arguments[i]);
-		}
-		if (evtName) {
-			var fnList = this.eventList[evtName];
-			for (var i in fnList) {
-				if (typeof fnList[i] == 'function') {
-					fnList[i].apply(this, args);
-				}
-			}
-		}
+  if (evtName) {
+	this.eventList = this.eventList || [];
+	var args = [];
+	for (var i = 1; i < arguments.length; i++) {
+	  args.push(arguments[i]);
 	}
+	if (evtName) {
+	  var fnList = this.eventList[evtName];
+	  for (var i in fnList) {
+		if (typeof fnList[i] == 'function') {
+		  fnList[i].apply(this, args);
+		}
+	  }
+	}
+  }
 }
 
 var jquery_lang_js = function () {
-	this.events = new IgeEventsLite();
-	
-	this.on = this.events.on;
-	this.emit = this.events.emit;
-	
-	return this;
+  this.events = new IgeEventsLite();
+  
+  this.on = this.events.on;
+  this.emit = this.events.emit;
+  
+  return this;
 }
 
 jquery_lang_js.prototype.lang = {};
@@ -39,140 +39,138 @@ jquery_lang_js.prototype.defaultLang;
 jquery_lang_js.prototype.currentLang;
 
 jquery_lang_js.prototype.run = function (defLang) {
-	if(defLang){
-		this.defaultLang = defLang;
-	}
-	if(!this.currentLang){
-		this.currentLang=this.defaultLang;
-	}
-	var langElems = $('[jql]');
-	var elemsLength = langElems.length;
-	
-	while (elemsLength--) {
-		var elem = langElems[elemsLength];
-		var elemType = elem.tagName;
-		if(elemType!='HTML'){
-			var langElem = $(elem);
-			if (langElem.is("input")) {
-				// An input element
-				switch (langElem.attr('type')) {
-					case 'button':
-					case 'submit':
-						/*
-						quando cambio pagine con jqm, questo valore e' gia' stato trasformato nella stringa effettiva!!!
-						*/
-						if(!langElem.data('deftext')){
-							langElem.data('deftext', langElem.val());
-						}
-					break;
-					case 'password':
-					case 'text':
-						// Check for a placeholder text value
-						var plText = langElem.attr('placeholder');
-						if (plText && !langElem.data('deftext')) {
-							langElem.data('deftext', plText);
-						}
-					break;
-				}
-			} else {
-				// Not an input element
-				if(!langElem.data('deftext')){
-					langElem.data('deftext', langElem.html());
-				}
+  if(defLang){
+	this.defaultLang = defLang;
+  }
+  if(!this.currentLang){
+	this.currentLang=this.defaultLang;
+  }
+  var langElems = $('[jql]');
+  var elemsLength = langElems.length;
+  
+  while (elemsLength--) {
+	var elem = langElems[elemsLength];
+	var elemType = elem.tagName;
+	if(elemType!='HTML'){
+	  var langElem = $(elem);
+	  if (langElem.is("input")) {
+		// An input element
+		switch (langElem.attr('type')) {
+		  case 'button':
+		  case 'submit':
+			// when I change page with jQueryMobile, this value has already been changed into the actual string
+			if(!langElem.data('deftext')){
+			  langElem.data('deftext', langElem.val());
 			}
+			break;
+		  case 'password':
+		  case 'text':
+			// Check for a placeholder text value
+			var plText = langElem.attr('placeholder');
+			if (plText && !langElem.data('deftext')) {
+			  langElem.data('deftext', plText);
+			}
+			break;
 		}
+	  } else {
+		// Not an input element
+		if(!langElem.data('deftext')){
+		  langElem.data('deftext', langElem.html());
+		}
+	  }
 	}
-	
-	// Now that the language system is setup, check
-	// if there is a current language and switch to it
-	if (localStorage) {
-		var lsLang = localStorage.getItem('langJs_currentLang');
-		if (lsLang) {
-			this.currentLang = lsLang;
-			this.change(lsLang);
-		} else {
-			this.currentLang = this.defaultLang;
-			this.change(this.defaultLang);
-		}
+  }
+  
+  // Now that the language system is setup, check
+  // if there is a current language and switch to it
+  if (localStorage) {
+	var lsLang = localStorage.getItem('langJs_currentLang');
+	if (lsLang) {
+	  this.currentLang = lsLang;
+	  this.change(lsLang);
 	} else {
-		this.change(this.defaultLang);
+	  this.currentLang = this.defaultLang;
+	  this.change(this.defaultLang);
 	}
+  } else {
+	this.change(this.defaultLang);
+  }
 }
 
 jquery_lang_js.prototype.loadPack = function (packPath) {
-	$('<script type="text/javascript" charset="utf-8" src="' + packPath + '" />').appendTo("head");
+  $('<script type="text/javascript" charset="utf-8" src="' + packPath + '" />').appendTo("head");
 }
-	
+
 jquery_lang_js.prototype.change = function (lang) {
-	//console.log('Changing language to ' + lang);
-	if (this.currentLang != lang) { 
-		this.update(lang); 
-	}
-	this.currentLang = lang;
-	
-	// Get the page HTML
-	var langElems = $('[jql]');
-		
-	if (this.lang[lang]) {
-		var elemsLength = langElems.length;
-		while (elemsLength--) {
-			var elem = langElems[elemsLength];
-			var langElem = $(elem);
-			if (langElem.data('deftext')) {
-				if (langElem.is("input")) {
-					// An input element
-					switch (langElem.attr('type')) {
-						case 'button':
-						case 'submit':
-							// A button or submit, change the value attribute
-							var currentText = langElem.val();
-							var defaultLangText = langElem.data('deftext');
-							
-							var newText = this.lang[lang][defaultLangText] || currentText;
-							var newHtml = currentText.replace(currentText, newText);
-							langElem.val(newHtml);
-							break;
-						case 'password':
-						case 'text':
-							// Check for a placeholder text value
-							var currentText = langElem.attr('placeholder');
-							var defaultLangText = langElem.data('deftext');
-							
-							var newText = this.lang[lang][defaultLangText] || currentText;
-							var newHtml = currentText.replace(currentText, newText);
-							langElem.attr('placeholder', newHtml);
-							break;
-					}
-				} else {
-					// Not an input element
-					var currentText = langElem.html();
-					var defaultLangText = langElem.data('deftext');
-					
-					var dlt = this.lang[lang][defaultLangText];
-					var newText = dlt || currentText;
-					var newHtml = currentText.replace(currentText, newText);
-					langElem.html(newHtml);
-				}
-			} else {
-				//console.log('No language data for element... have you executed .run() first?');
-			}
+  //console.log('Changing language to ' + lang);
+  if (this.currentLang != lang) {
+	this.update(lang);
+  }
+  this.currentLang = lang;
+  
+  // Get the page HTML
+  var langElems = $('[jql]');
+  
+  if (this.lang[lang]) {
+	var elemsLength = langElems.length;
+	while (elemsLength--) {
+	  var elem = langElems[elemsLength];
+	  var langElem = $(elem);
+	  if (langElem.data('deftext')) {
+		if (langElem.is("input")) {
+		  // An input element
+		  switch (langElem.attr('type')) {
+			case 'button':
+			case 'submit':
+			  // A button or submit, change the value attribute
+			  var currentText = langElem.val();
+			  var defaultLangText = langElem.data('deftext');
+			  
+			  var newText = this.lang[lang][defaultLangText] || currentText;
+			  var newHtml = currentText.replace(currentText, newText);
+			  langElem.val(newHtml);
+			  break;
+			case 'password':
+			case 'text':
+			  // Check for a placeholder text value
+			  var currentText = langElem.attr('placeholder');
+			  var defaultLangText = langElem.data('deftext');
+			  
+			  var newText = this.lang[lang][defaultLangText] || currentText;
+			  var newHtml = currentText.replace(currentText, newText);
+			  langElem.attr('placeholder', newHtml);
+			  break;
+		  }
+		} else {
+		  // Not an input element
+		  var currentText = langElem.html();
+		  var defaultLangText = langElem.data('deftext');
+		  
+		  var dlt = this.lang[lang][defaultLangText];
+		  var newText = dlt || currentText;
+		  var newHtml = currentText.replace(currentText, newText);
+		  langElem.html(newHtml);
 		}
-	} else {
-		console.log('Cannot switch language, no language pack defined for "' + lang + '"');
+	  } else {
+		//console.log('No language data for element... have you executed .run() first?');
+	  }
 	}
+  } else {
+	console.log('Cannot switch language, no language pack defined for "' + lang + '"');
+  }
 }
 
 jquery_lang_js.prototype.convert = function (text, lang) {
-	if (lang) {
-		return this.lang[lang][text];
-	} else {
-		return this.lang[this.currentLang][text];
-	}
+  if (lang) {
+	return this.lang[lang][text];
+  } else {
+	return this.lang[this.currentLang][text];
+  }
 }
 
 jquery_lang_js.prototype.update = function (lang) {
-	if (localStorage) {
-		localStorage.setItem('langJs_currentLang', lang);
-	}
-	this.emit('update', lang);
+  if (localStorage) {
+	localStorage.setItem('langJs_currentLang', lang);
+  }
+  this.emit('update', lang);
 }
